465ebc516faf482b3c30f3b9d59bdf13c285c9de
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-03-15 17:30:24 +0530

    nav mobile

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import { Image, Popover, Button, Text, Modal, useModal } from "@nextui-org/react";
+import { Image, Popover, Button, Text, Modal, useModal, Navbar, Card  } from "@nextui-org/react";
 import Link from 'next/link';
 
 export default function Nav(props) {
 
+
+  const { setVisible, bindings } = useModal();
+
   return (
+
+    
     <nav className='flex justify-between h-16'>
     
         <div className='pt-4'>
@@ -19,6 +24,30 @@ export default function Nav(props) {
             <li className={`px-3 rounded-lg ${props.kcolor} font-medium hover:bg-slate-100 hover:text-slate-900`}><Link href='/know'>Know More</Link></li>
             <li className={`px-3 rounded-lg ${props.ccolor} font-medium hover:bg-slate-100 hover:text-slate-900`}><Link href='/contact'>Contact</Link></li>   
             </ul>
+        </div>
+        <div className='lg:invisible lg:hidden visible p-3'>
+        <Button color={''} auto flat onPress={() => setVisible(true)}>
+        <Image src='hamburger.svg' alt='.'/>
+      </Button>
+      <Modal
+        scroll
+        fullScreen
+        closeButton
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        {...bindings}
+      >
+ 
+        <Modal.Body>
+          <Link href={'/'}><Text className='bg-white p-10 text-center font-bold text-black text-2xl' id="modal-description">Home</Text></Link>
+          <Link href={'/about'}><Text color='white' className='bg-teal-800 font-bold p-16 text-center text-2xl -mr-6 -ml-6 ' id="modal-description">About</Text></Link>
+          <Link href={'/work'}><Text color='white' className='bg-yellow-600 font-bold p-16 -mt-4 text-center text-2xl -mr-6 -ml-6 ' id="modal-description">Works</Text></Link>
+          <Link href={'/know'}><Text color='white' className='bg-blue-600 font-bold p-16 -mt-4 text-center text-2xl -mr-6 -ml-6 ' id="modal-description">Know More</Text></Link>
+          <Link href={'/contact'}><Text color='white' className='bg-yellow-800 font-bold p-16 -mt-4 text-center text-2xl -mr-6 -ml-6 ' id="modal-description">Contact</Text></Link>
+        </Modal.Body>
+       
+      </Modal>
+    
         </div>
         
     </nav>
