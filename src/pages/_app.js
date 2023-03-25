8d0d0ea198a9c5e9abf9b6afc8a95ab460acb7e4
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-03-25 20:29:07 +0530

    .

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,5 @@
 import '@/styles/globals.css'
-import { NextUIProvider } from '@nextui-org/react';
-import Script from "next/script";
-
-// ...
-
-// This code belongs ABOVE the <Head> tag in your _app.js file's return.
-
-{/* Global Site Tag (gtag.js) - Google Analytics */}
-
-
-
 
 export default function App({ Component, pageProps }) {
-
-  return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-EZ7P9312VS`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-EZ7P9312VS');
-        `}
-      </Script>
-
-      <NextUIProvider><Component {...pageProps} /></NextUIProvider>
-    </>
-  );
-} 
+  return <Component {...pageProps} />
+}
