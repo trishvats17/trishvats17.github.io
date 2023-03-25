63d689f301c3d23d15c6d0920b67cf7ca83462c5
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-03-25 20:27:39 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,16 @@ export default function Home() {
   return (
     <>
       <Head>
+        
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-EZ7P9312VS"></script>
+  <script>
+    {`window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-EZ7P9312VS');`}
+  </script>
+
         <title>Trishabh Vats</title>
         <meta name="description" content="Trisabh's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
